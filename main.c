73c013e1a2564afd690f9784f70833c76869759a
imcort <179376503@qq.com>
2019-08-20 16:48:05 +0800

    Decrease ADV interval
    some software may timeout when using 4 seconds

--- main.c
+++ main.c
@@ -100,7 +100,7 @@ static const char DEVICE_NAME[] =       {0xe7, 0x82, 0xb9,
 																					0xf0, 0x9f, 0x98, 0x9d}; /**< Name of device. Will be included in the advertising data. */
 
 #define MANUFACTURER_NAME               "TianJinUniversity"                       /**< Manufacturer. Will be passed to Device Information Service. */
-#define APP_ADV_INTERVAL                4000                                         /**< The advertising interval (in units of 0.625 ms. This value corresponds to 187.5 ms). */
+#define APP_ADV_INTERVAL                3000                                         /**< The advertising interval (in units of 0.625 ms. This value corresponds to 187.5 ms). */
 #define APP_ADV_DURATION                0                                       /**< The advertising duration (180 seconds) in units of 10 milliseconds. */
 
 #define APP_BLE_OBSERVER_PRIO           3                                           /**< Application's BLE observer priority. You shouldn't need to modify this value. */
